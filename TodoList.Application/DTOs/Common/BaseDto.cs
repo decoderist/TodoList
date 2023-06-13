ab9196d8b2a3aa42f9d13689a6b32dcc971cd94c
Amin Masoumkhani <decoderist@gmail.com>
2023-06-13 17:18:31 +0330

    Implementation AutoMapper, Create DTOs

---
+++ TodoList.Application/DTOs/Common/BaseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList.Application.DTOs.Common
+{
+    public abstract class BaseDto
+    {
+        [Required, Key]
+        public int Id { get; set; }
+        [Required]
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ TodoList.Application/DTOs/TodoItemDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.Application.DTOs.Common;
+
+namespace TodoList.Application.DTOs
+{
+    public class TodoItemDto:BaseDto
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [MaxLength(200)]
+        [MinLength(15)]
+        [DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+
+        public bool Done { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        [Column("Tags")]
+        [MaxLength(3)]
+        public IEnumerable<string> Tags { get; set; }
+    }
+}
